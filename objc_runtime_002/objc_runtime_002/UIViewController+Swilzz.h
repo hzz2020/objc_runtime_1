fec545a40cd2edf0b2339479311684caf0f11dce
HEXSZ <HEXSZ@localhost>
2016-03-22 12:03:41 +0800

    runtime学习之 Associated Objects 三方法 void objc_setAssociatedObject ( id object, const void *key, id value, objc_AssociationPolicy policy ); 用于给对象添加关联对象，传入 nil 则可以移除已有的关联对象； id objc_getAssociatedObject ( id object, const void *key ); 用于获取关联对象； void objc_removeAssociatedObjects ( id object ); 用于移除一个对象的所有关联对象。

---
+++ objc_runtime_002/objc_runtime_002/UIViewController+Swilzz.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+Swilzz.h
+//  objc_runtime_002
+//
+//  Created by HEXSZ on 16/3/22.
+//  Copyright © 2016年 大辉郎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (Swilzz)
+
+@end
---
+++ objc_runtime_002/objc_runtime_002/UIViewController+Swilzz.m
@@ -0,0 +1,13 @@
+//
+//  UIViewController+Swilzz.m
+//  objc_runtime_002
+//
+//  Created by HEXSZ on 16/3/22.
+//  Copyright © 2016年 大辉郎. All rights reserved.
+//
+
+#import "UIViewController+Swilzz.h"
+
+@implementation UIViewController (Swilzz)
+
+@end
